49d72b8b8123fec693325b695ec8919848a0cb2e
priyanshusuman9 <priyanshusuman947@gmail.com>
2022-12-20 15:02:01 +0530

    [Priyanshu Suman] Employee Wage Problem

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,4 +1,21 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static int days = 0;
+    static double value = Math.floor(Math.random() * 10) % 2;
+
+    public static void main(String[] args) {
+        Uc1.Present();
+
+
+    }
+    static void Present(){
+        if (value == days  ){
+            System.out.println("Wage is Present " );
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+    }
+
 }
